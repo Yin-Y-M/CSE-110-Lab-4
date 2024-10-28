8b7ac1266c5e8d96492fd4698cc536357065092f
Yin-Y-M <117802747+Yin-Y-M@users.noreply.github.com>
2024-10-28 13:52:58 -0700

    Change to the right version

--- src/context/AppContext.test.tsx
+++ src/context/AppContext.test.tsx
@@ -27,9 +27,9 @@ describe("Budget Tracker Application", () => {
     //Verify the new expense is added
     expect(screen.getByText("MyExpense")).toBeInTheDocument();
     //correct version: 
-    //expect(screen.getByText("$200")).toBeInTheDocument();
+    expect(screen.getByText("$200")).toBeInTheDocument();
     //test for incorrect
-    expect(screen.getByText("$100")).toBeInTheDocument();
+    //expect(screen.getByText("$100")).toBeInTheDocument();
     // Check total expenses and remaining budget
     
     expect(screen.getByTestId("remaining-alert")).toHaveTextContent("Remaining: $800"); // Assuming initial budget is $1000
